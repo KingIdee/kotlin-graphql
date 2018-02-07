99b263cd9922ee35fea2ffebc93a365a1a0ddf3e
Idorenyin Obong <idee4ril@gmail.com>
2018-02-07 15:16:02 +0100

    initial commit

---
+++ app/src/main/java/com/example/android/kotlingraphql/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.android.kotlingraphql
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.OkHttpClient
+import android.support.v7.widget.GridLayoutManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // Initialize apolloclient with our server url
+        val apolloClient = ApolloClient.builder()
+                .serverUrl("https://api.graph.cool/simple/v1/cjcujyx5f07p801916rjjigz7")
+                .okHttpClient(OkHttpClient.Builder().build()).build()
+
+        // query our server for response
+        apolloClient.query(MovieQuery.builder().build())
+                .enqueue(object : ApolloCall.Callback<MovieQuery.Data>() {
+
+            override fun onResponse(response: Response<MovieQuery.Data>) {
+                Log.d("TAG", response.data().toString())
+                runOnUiThread {
+                    setupRecyclerView(response.data()!!.allMovies())
+                }
+            }
+
+            override fun onFailure(e: ApolloException) {
+                Log.d("TAG", e.message)
+            }
+
+        })
+
+    }
+
+    private fun setupRecyclerView(list: List<MovieQuery.AllMovie>) {
+        val adapter = MoviesAdapter(list)
+        recyclerView.layoutManager= GridLayoutManager(this, 2)
+        recyclerView.adapter = adapter
+    }
+
+
+}
---
+++ app/src/main/java/com/example/android/kotlingraphql/MoviesAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.android.kotlingraphql
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import java.util.*
+
+/**
+ * Created by idorenyin on 2/2/18.
+ */
+
+class MoviesAdapter(private var movieList: List<MovieQuery.AllMovie>) : RecyclerView.Adapter<MoviesAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+
+        val view = LayoutInflater.from(parent!!.context).inflate(R.layout.single_item,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return movieList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        val current = movieList[position]
+        holder!!.movieTextview.text = current.title()
+        Glide.with(holder.itemView).load(current.imageUrl).into(holder.movieImage)
+
+    }
+
+    inner class ViewHolder(itemView: View?): RecyclerView.ViewHolder(itemView) {
+        var movieTextview: TextView = itemView!!.findViewById(R.id.movie_title)
+        var movieImage: ImageView= itemView!!.findViewById(R.id.movie_image)
+    }
+}
